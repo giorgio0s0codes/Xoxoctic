0f8f719fecccf0ad65b167a35a81788ee6b1b570
giorgio0s0codes <giorgio.oso@outlook.com>
2025-05-04 13:59:29 -0600

    This is the base Set Up for my app. Now on to write UIextension

---
+++ Xoxoctic/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  Xoxoctic
+//
+//  Created by Giorgio Oso on 04/05/25.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
