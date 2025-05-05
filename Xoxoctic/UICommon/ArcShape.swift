90368209d60930b238fe593bd1e9f1a7e23c83c8
giorgio0s0codes <giorgio.oso@outlook.com>
2025-05-04 19:36:19 -0600

    Stylish Arc included

---
+++ Xoxoctic/UICommon/ArcShape.swift
@@ -0,0 +1,18 @@
+//
+//  ArcShape.swift
+//  Xoxoctic
+//
+//  Created by Giorgio Oso on 04/05/25.
+//
+
+import SwiftUI
+
+struct ArcShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArcShape()
+}
