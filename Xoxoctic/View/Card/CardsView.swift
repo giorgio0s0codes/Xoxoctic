83e46c7f5599d154d7526da8e59c67d565fdffc9
giorgio0s0codes <giorgio.oso@outlook.com>
2025-05-04 21:53:32 -0600

    Added a Settings View. Now to add cards.

---
+++ Xoxoctic/View/Card/CardsView.swift
@@ -0,0 +1,18 @@
+//
+//  CardsView.swift
+//  Xoxoctic
+//
+//  Created by Giorgio Oso on 04/05/25.
+//
+
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardsView()
+}
